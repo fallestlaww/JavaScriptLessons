8a90b7924ba56082f4f1318eef74c55fe8129e55
fallestlaww <qtensell1@gmail.com>
2023-03-20 00:47:27 +0200

    oop

---
+++ lesson-06-homework/prototype.js
@@ -0,0 +1,50 @@
+function alert() {
+    console.log("--------------------")
+}
+
+function Robot() {
+    let self = this
+    Robot.prototype.work = function () {
+        console.log("Я Robot - я просто працюю")
+    }
+}
+
+function CoffeeRobot() {
+    Robot.call(this)
+    let self = this
+    CoffeeRobot.prototype.work = function () {
+        console.log("Я CoffeRobot – я варю каву")
+    }
+}
+
+function RobotDancer() {
+    Robot.call(this)
+    let self = this
+    RobotDancer.prototype.work = function () {
+        console.log("Я RobotDancer – я просто танцюю")
+    }
+}
+
+function RobotCoocker() {
+    Robot.call(this)
+    let self = this
+    RobotCoocker.prototype.work = function () {
+        console.log("Я RobotCoocker – я просто готую")
+    }
+}
+
+let robot = new Robot()
+let coffeerobot = new CoffeeRobot()
+let dancerrobot = new RobotDancer()
+let coockerrobot = new RobotCoocker()
+
+robot.work()
+coffeerobot.work()
+dancerrobot.work()
+coockerrobot.work()
+
+let robots = [robot, coffeerobot, dancerrobot, coockerrobot]
+alert()
+for (let i = 0; i < robots.length; i++) {
+    robots[i].work()
+}
